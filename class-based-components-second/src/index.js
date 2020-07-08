65b34fea88f02186cccbeb3d1a0411cab08cd74c
Garia, Pawan (GfK) <Pawan.Garia@gfk.com>
2020-07-09 00:16:30 +0200

    Error Message and Latitude based on if condition in Render method.

--- class-based-components-second/src/index.js
+++ class-based-components-second/src/index.js
@@ -4,25 +4,33 @@ import ReactDOM from 'react-dom';
 // // React Class Based component  => Print and use the Geolocation API to Get the Position.
 // With a render() method and Extending the Class to React.component class.
 class App extends React.Component {
-    
+
     //In React the constructor needs to be called its not a default lie java ScriptProcessorNode.
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {lat: null};
+        // Create 2 states Lat and error messages and fill at run time.
+        this.state = { lat: null, errorMessage: null };
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 //Call Set State always to change state.
-                this.setState({lat: position.coords.latitude});
+                this.setState({ lat: position.coords.latitude });
             },
-            err => console.log(err)
-           );
+            err => {
+                this.setState({ errorMessage: err.message })
+            }
+        );
     }
 
     // React says we have to define the render() method. or rect will give error
-   render(){
-    
-  return <div> Latitude :{this.state.lat} </div>;
+    render() {
+       if(this.state.errorMessage && !this.state.lat){
+           return <div> Error: {this.state.errorMessage} </div>;
+       }    
+       if(!this.state.errorMessage && this.state.lat){
+           return <div> Latitude: {this.state.lat}</div>;
+       }
+       return <div>Loading</div>;
     }
 };
 
-ReactDOM.render(<App/>,document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));

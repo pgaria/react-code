da878319ca63a94337829c4e956dce220dba777e
Garia, Pawan (GfK) <Pawan.Garia@gfk.com>
2020-07-08 23:54:08 +0200

    Using Class and set State we change the Latitude and Print on Browser

--- class-based-components-second/src/index.js
+++ class-based-components-second/src/index.js
@@ -4,12 +4,24 @@ import ReactDOM from 'react-dom';
 // // React Class Based component  => Print and use the Geolocation API to Get the Position.
 // With a render() method and Extending the Class to React.component class.
 class App extends React.Component {
+    
+    //In React the constructor needs to be called its not a default lie java ScriptProcessorNode.
+    constructor(props){
+        super(props);
+        this.state = {lat: null};
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                //Call Set State always to change state.
+                this.setState({lat: position.coords.latitude});
+            },
+            err => console.log(err)
+           );
+    }
+
+    // React says we have to define the render() method. or rect will give error
    render(){
-    window.navigator.geolocation.getCurrentPosition(
-     position => console.log(position),
-     err => console.log(err)
-    );
-  return <div> Latitude : </div>;
+    
+  return <div> Latitude :{this.state.lat} </div>;
     }
 };
 

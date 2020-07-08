f8d7260b4ecc0f1d38e5a63f91dfb50a63deb350
Garia, Pawan (GfK) <Pawan.Garia@gfk.com>
2020-07-08 23:29:51 +0200

    Convert functional App Component to Class App Component

--- class-based-components-second/src/index.js
+++ class-based-components-second/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Print and use the Geolocation API to Get the Position.
-const App = () => {
-   
+// // React Class Based component  => Print and use the Geolocation API to Get the Position.
+// With a render() method and Extending the Class to React.component class.
+class App extends React.Component {
+   render(){
     window.navigator.geolocation.getCurrentPosition(
      position => console.log(position),
      err => console.log(err)
     );
-  return <div> Hi There! </div>;
-
+  return <div> Latitude : </div>;
+    }
 };
 
 ReactDOM.render(<App/>,document.querySelector('#root'));

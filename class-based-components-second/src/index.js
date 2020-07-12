4b4d36325acd65fb5289962dfb3313b2677fb2e2
Garia, Pawan (GfK) <Pawan.Garia@gfk.com>
2020-07-12 21:58:22 +0200

    Adding LifeCycle Methods and Removed the Data Loading from Constructor to the ComponentDidMount method

--- class-based-components-second/src/index.js
+++ class-based-components-second/src/index.js
@@ -8,21 +8,35 @@ class App extends React.Component {
     //In React the constructor needs to be called its not a default lie java ScriptProcessorNode.
     constructor(props) {
         super(props);
+        console.log("In constructor");
         // Create 2 states Lat and error messages and fill at run time.
         this.state = { lat: null, errorMessage: null };
+    }
+    
+    // Now Loading the Data in this method and removed from Constructor.
+    //Calls for the Render method after that.
+    componentDidMount(){
+        console.log("My Component Rendered, in componentDidMount method");
         window.navigator.geolocation.getCurrentPosition(
-            position => {
+            position =>
                 //Call Set State always to change state.
-                this.setState({ lat: position.coords.latitude });
-            },
-            err => {
+                this.setState({ lat: position.coords.latitude }),
+            err =>
                 this.setState({ errorMessage: err.message })
-            }
         );
     }
 
+    //Calls for the Render method after that.
+    componentDidUpdate(){
+        console.log("Component  Did update");
+    }
+
+
     // React says we have to define the render() method. or rect will give error
+    // Adding the If condition to show Error or Latitude based on what is available and return.
+    // Render method should be just used for returning the JSX only, not API calls like we do in constructor..
     render() {
+        console.log("In Render");
        if(this.state.errorMessage && !this.state.lat){
            return <div> Error: {this.state.errorMessage} </div>;
        }    

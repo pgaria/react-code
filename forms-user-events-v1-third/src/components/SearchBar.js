77322d4071bc33731713f5d5e17e9c691f8e1127
Garia, Pawan (GfK) <Pawan.Garia@gfk.com>
2020-07-13 22:01:23 +0200

    Event Handling on the Input tag for search. Added the onChange call back function as well

--- forms-user-events-v1-third/src/components/SearchBar.js
+++ forms-user-events-v1-third/src/components/SearchBar.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    
+    //Create a method to read the Text user is making or event and refer this method in the onChange Attribute/CallBack function of the Input TAG below.
+    //Means whenever the user input in our input Field this Method should be called. and this method receives and JS Event object property.
+    onInputChange(event) {
+     console.log(event.target.value);
+    }
+
+
 
     render() {
         return (
@@ -8,7 +16,7 @@ class SearchBar extends React.Component {
                 <form className="ui form">
                     <div className="field">
                         <label> Image Search: </label>
-                        <input type="text" />
+                        <input type="text" onChange={this.onInputChange} />
                     </div>
                 </form>
             </div>

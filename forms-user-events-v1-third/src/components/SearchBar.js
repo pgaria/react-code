e26a993843ad7335629d547d3eba581909036dbc
Garia, Pawan (GfK) <Pawan.Garia@gfk.com>
2020-07-13 22:42:39 +0200

    Create and Set State in React Component and Controlled Elements with Value

--- forms-user-events-v1-third/src/components/SearchBar.js
+++ forms-user-events-v1-third/src/components/SearchBar.js
@@ -1,22 +1,19 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    
-    //Create a method to read the Text user is making or event and refer this method in the onChange Attribute/CallBack function of the Input TAG below.
-    //Means whenever the user input in our input Field this Method should be called. and this method receives and JS Event object property.
-    onInputChange(event) {
-     console.log(event.target.value);
-    }
 
+    //Creating a State which can manage the Text Entered by the User.
+    state = { userText: '' };
 
-
+    // Method no need to create in the onChange Event and can be Directly used with => expression.
+    //Now we change and set the State in the controlled Elemenets as the Value is Stored by the Rect Componment not the HTML DOM.
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label> Image Search: </label>
-                        <input type="text" onChange={this.onInputChange} />
+                        < input type="text" value={this.state.userText} onChange={event => this.setState({ userText: event.target.value })} />
                     </div>
                 </form>
             </div>
